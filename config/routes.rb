36cf91ebad350717340b6377f9821eca480f4792
Billy Paniza <fhubil27@gmail.com>
2021-04-27 19:51:34 +0800

    MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,29 @@
 Rails.application.routes.draw do
 
 	root 'pages#home'
+
+get '/get-evaluated', to: 'pages#evaluation'
+  get '/contact', to: 'pages#contact'
+  
+  scope '/about-us' do
+    get '/', to: 'aboutus#about', as: :about
+    get '/base-assesstment', to: 'aboutus#base'
+    get '/athlete-assesstment', to: 'aboutus#athlete'
+    get '/training-tools', to: 'aboutus#tools'
+  end
+
+  scope '/our-team' do
+    get '/board', to: 'ourteam#board'
+    get '/directors-operations', to: 'ourteam#directors'
+    get '/baseball-team', to: 'ourteam#baseball'
+    get '/softball-team', to: 'ourteam#softball'
+    get '/football-team', to: 'ourteam#football'
+    get '/volleyball-team', to: 'ourteam#volleyball'
+    get '/rugby-team', to: 'ourteam#rugby'
+  end
+  
+  resources :blogs, only: :index
+  #temporary route
+  get '/blogs/1', to: 'blogs#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
